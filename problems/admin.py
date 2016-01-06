80d14f7c6d9f9dc4042de8e34e08a17774ef994e
cookiebus <1200012757@pku.edu.cn>
2016-01-07 02:12:40 +0800

    update

--- problems/admin.py
+++ problems/admin.py
@@ -3,7 +3,7 @@ from problems.models import Problem
 
 # Register your models here.
 class ProblemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'problem_image', 'description', 'up')
+    list_display = ('id', 'title', 'user', 'problem_image', 'description', 'up', 'X', 'Y')
     ordering = ('-id', )
     filter_horizontal = ('tags', )
 
